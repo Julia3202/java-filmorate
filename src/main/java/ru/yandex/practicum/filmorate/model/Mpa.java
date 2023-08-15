85c0ac8e22f70c0d5dc548fc254f9c708ea1c525
Julia3202 <123663917+Julia3202@users.noreply.github.com>
2023-08-15 10:35:11 +0300

    Update Mpa.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -5,13 +5,9 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Mpa {
 
     private Integer id;
     private String name;
-
-    public Mpa(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
